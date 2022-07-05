317689592c92e4d86255a5dc2494c299d2d9f66f
leland knight <lknite@aarr.xyz>
2022-07-05 16:29:36 -0400

    initial checkin of custom server access urls

--- settings/network_custom_server_access_urls/network_custom_server_access_urls.sh
+++ settings/network_custom_server_access_urls/network_custom_server_access_urls.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# $1 = the passed in parameter
+#      a string of urls to register as how this server can be accessed (plex mobile will use this value)
 
-echo "show parameters"
-echo $0
-echo $1
+# - searches for the 'AcceptedEULA' attribute to identify the Preferences xml object
+# - if 'customConnections' attribute is present then set to value of passed in parameter, otherwise add it
+xmlstarlet edit --inplace --update "//*[@AcceptedEULA=\"1\"]/@customConnections" --value "$1" --insert "//*[@AcceptedEULA=\"1\"][not(@customConnections)]" --type attr -n customConnections -v "$1" /config/Library/Application Support/Plex Media Server/Preferences.xml
