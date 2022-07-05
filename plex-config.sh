947da2fe639820cc357bc46b0493decafdc86a90
leland knight <lknite@aarr.xyz>
2022-07-05 14:47:21 -0400

    testing with first call to a script

--- plex-config.sh
+++ plex-config.sh
@@ -5,8 +5,30 @@ SETTINGS=`printenv | grep PLEX_CONFIG`
 
 for SETTING in $SETTINGS
 do
-  NEXT=`echo $SETTING | awk '{split($0,a,"="); print a[1]}'`
-  echo "[$NEXT]"
-  echo "Executing ./settings/$(echo $NEXT | tr '[:upper:]' '[:lower:]').sh"
+  ##
+  # parse off setting
 
+  # get name of the setting (from the name=value string)
+  NEXT=`echo $SETTING | awk '{split($0,a,"="); print a[1]}'`
+  
+  ##
+  # get script path
+
+  # remove the PLEX_CONFIG_ prepended value
+  SCRIPT=$NEXT
+  SCRIPT=${SCRIPT#*_} 
+  SCRIPT=${SCRIPT#*_} 
+
+  # ensure lowercase
+  SCRIPT="$(echo $SCRIPT | tr '[:upper:]' '[:lower:]')"
+
+  # form path
+  SCRIPTPATH="./settings/${SCRIPT}/${SCRIPT}.sh"
+
+  ##
+  # display & execute
+
+  # call the script which knows how to set this value
+  echo "Configuring $SCRIPT ..."
+  $SCRIPTPATH "${!NEXT}"
 done
---
+++ settings/network_custom_server_access_urls/network_custom_server_access_urls.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "show parameters"
+echo $0
+echo $1
