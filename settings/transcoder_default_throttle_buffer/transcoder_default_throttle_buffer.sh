33ff1d74bf9a8de8f1c41c3fec83d7ad7166b92b
leland knight <lknite@aarr.xyz>
2022-07-05 16:25:49 -0400

    improve comment

--- settings/transcoder_default_throttle_buffer/transcoder_default_throttle_buffer.sh
+++ settings/transcoder_default_throttle_buffer/transcoder_default_throttle_buffer.sh
@@ -3,5 +3,5 @@
 #      a number, by default 60, though 600 is recommended
 
 # - searches for the 'AcceptedEULA' attribute to identify the Preferences xml object
-# - if 'TranscoderThrottleBuffer' attribute is present then set to value of passed in parameter
+# - if 'TranscoderThrottleBuffer' attribute is present then set to value of passed in parameter, otherwise add it
 xmlstarlet edit --inplace --update "//*[@AcceptedEULA=\"1\"]/@TranscoderThrottleBuffer" --value "$1" --insert "//*[@AcceptedEULA=\"1\"][not(@TranscoderThrottleBuffer)]" --type attr -n TranscoderThrottleBuffer -v "$1" /config/Library/Application Support/Plex Media Server/Preferences.xml
