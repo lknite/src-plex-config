c339241e3299a7990022a85468d059225905b8f0
leland knight <lknite@aarr.xyz>
2022-07-05 16:11:20 -0400

    update comments

--- settings/network_enable_relay/network_enable_relay.sh
+++ settings/network_enable_relay/network_enable_relay.sh
@@ -2,7 +2,8 @@
 # $1 = the passed in parameter
 #      - accepts 'true' or 'false'
 
-# if attribute is present then set to 0 or 1 based on parameter
+# - searches for the 'AcceptedEULA' attribute to identify the Preferences xml object
+# - if 'RelayEnabled' attribute is present then set to 0 or 1 based on passed in parameter
 if [ $1 = "true" ]; then
   xmlstarlet edit --inplace --update "//*[@AcceptedEULA=\"1\"]/@RelayEnabled" --value "1" --insert "//*[@AcceptedEULA=\"1\"][not(@RelayEnabled)]" --type attr -n RelayEnabled -v "1" /config/Library/Application Support/Plex Media Server/Preferences.xml
 else
