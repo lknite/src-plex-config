9667be8ca84daa8e6b5f19c2aea8a698b983b2a7
leland knight <lknite@aarr.xyz>
2022-07-05 16:24:55 -0400

    initial checkin of script to configure transcoder default throttle buffer

--- settings/transcoder_default_throttle_buffer/transcoder_default_throttle_buffer.sh
+++ settings/transcoder_default_throttle_buffer/transcoder_default_throttle_buffer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# $1 = the passed in parameter
+#      a number, by default 60, though 600 is recommended
 
-echo "show parameters"
-echo $0
-echo $1
+# - searches for the 'AcceptedEULA' attribute to identify the Preferences xml object
+# - if 'TranscoderThrottleBuffer' attribute is present then set to value of passed in parameter
+xmlstarlet edit --inplace --update "//*[@AcceptedEULA=\"1\"]/@TranscoderThrottleBuffer" --value "$1" --insert "//*[@AcceptedEULA=\"1\"][not(@TranscoderThrottleBuffer)]" --type attr -n TranscoderThrottleBuffer -v "$1" /config/Library/Application Support/Plex Media Server/Preferences.xml
