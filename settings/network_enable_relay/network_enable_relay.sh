a388ed88206fbef63c0593c7a7d00ecc89282b4c
leland knight <lknite@aarr.xyz>
2022-07-05 16:08:50 -0400

    initial checkin of configuration item network_enable_relay

--- settings/network_enable_relay/network_enable_relay.sh
+++ settings/network_enable_relay/network_enable_relay.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# $1 = the passed in parameter
+#      - accepts 'true' or 'false'
 
-echo "show parameters"
-echo $0
-echo $1
+# if attribute is present then set to 0 or 1 based on parameter
+if [ $1 = "true" ]; then
+  xmlstarlet edit --inplace --update "//*[@AcceptedEULA=\"1\"]/@RelayEnabled" --value "1" --insert "//*[@AcceptedEULA=\"1\"][not(@RelayEnabled)]" --type attr -n RelayEnabled -v "1" /config/Library/Application Support/Plex Media Server/Preferences.xml
+else
+  xmlstarlet edit --inplace --update "//*[@AcceptedEULA=\"1\"]/@RelayEnabled" --value "0" --insert "//*[@AcceptedEULA=\"1\"][not(@RelayEnabled)]" --type attr -n RelayEnabled -v "0" /config/Library/Application Support/Plex Media Server/Preferences.xml
+fi
